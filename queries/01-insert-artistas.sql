9f877ac486e32eb5ef3834fb626a356b269f33b0
Ana Julia <ana.j.ferreira15@aluno.senai>
2025-09-15 10:42:51 -0300

    Feat: Cria o arquivo para selects

--- queries/01-insert-artistas.sql
+++ queries/01-insert-artistas.sql
@@ -9,3 +9,9 @@ CREATE TABLE artistas (
     musica_mais_ouvida VARCHAR(50),
     ativo BOOLEAN DEFAULT TRUE,
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo)
+VALUES
+('Coldplay', 'Banda', 4, 'Pop', 'Reino Unido', 1996, 'Yellow', TRUE)
+('Shawn Mendes', 'Cantor', 1, 'Pop', 'Canada', 2006, 'There s Nothing Holding me Back', TRUE )
+('Bruno Mars', 'Cantor', 1, 'Pop', 'Havai', '2010', 'When I was your man', TRUE);
